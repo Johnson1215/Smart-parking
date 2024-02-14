c88e62eb96db84c8f91747311d7e983426072c23
Johnson <91417125+Johnson1215@users.noreply.github.com>
2024-02-14 21:08:11 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+    <title>
+        INDEX
+
+    </title>
+
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+
+    <body>
+        <style>
+
+            .Plogo img{
+            width: 100px;
+            }
+            .Plogo{
+            margin-left: 20px;
+
+            }
+        </style>
+
+        <header id="header">
+            <div class="navbar navbar-inverse" role="banner">
+                <div class="container">
+                    <div class="navbar-header">
+
+                        <a class="navbar-brand" href="index.html"></a>
+                        <h4 class="Plogo"> <img  src="images/Parking_logo.jpg" alt="">
+                    <br> Smart Parking</h4>
+                            
+
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+        <style>
+        .slide-text{
+            margin-top: 150px;
+            color: black;
+            margin-left: 20px;
+            /*animation-name:mymove 5s;    
+            animation-fill-mode: forwards;*/
+        }/*
+        @keyframes mymove {
+            from {left: 0px;}
+            to {left: 300px;}
+        }*/
+        </style>
+        
+        <div class="container">
+            <div class="main-slider">
+                <div class="slide-text" id="movetex">
+                    <h1>“Creating a better future<br>
+                    Requires creativity in the present.”</h1>
+                    <!-- <p>― Matthew Goldfinger</p>-->
+                    <p>― UNIVOTEC</p>
+                </div>
+            </div>
+        </div>
+        
+        <style>
+        .btn-common{
+            text-decoration: none;
+            width: 80px;
+            height: 40px;
+            margin-left: 40px;
+            border-radius: 10px;
+        }
+        .btn-common:hover{
+            border-color: red;
+        }
+        .link{
+            text-decoration: none;
+        }
+        </style>
+        
+        <div>
+            <button class="btn-common"><a href="signup.php" class="link" >SIGN UP</a></button>
+            <button class="btn-common"><a href="login.php"  class="link" >LOG IN</a></button>
+        </div>
+        
+        <style>
+        .eye{
+            margin-left: 700px;
+            margin-top: -400px;
+            width: 500px;
+        }
+        </style>
+
+        <div>
+            <!-- <img src="images/200w.gif" class="car">-->
+            <img src="images/Eye.png" class="eye">
+        </div>
+    </body>
+</html>
